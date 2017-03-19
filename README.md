8cbc309d2788d411ca91792ac96f2d2a69872aef
LGB <lgblgblgb@gmail.com>
2017-03-19 18:51:04 +0100

    Tidy up a bit

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
+# Mega-65 utilities from LGB
+
+(C)2017 LGB (Gábor Lénárt) lgblgblgb@gmail.com
+See file LICENSE (GNU/GPL v3, or later).
+
 Various Mega-65 related utilities, and software, both of native (running on
 the M65 directly) and cross-platform stuffs. Currently this is more like some
 experiments rather than serious things though.
 
 For compilation, you may need CA65 assembler (from the CC65 suite) with
-up-to-date version (with 65CE02/4502 stuffs) and Linux/UNIX-like standard
+up-to-date version (with 65CE02/4510 stuffs) and Linux/UNIX-like standard
 evelopment environment with GNU Make, general UNIX utilities, Python, etc ...
+
+For quick testing of native tools, Xemu is highly recommended, installed
+in the default search path as xemu-mega65 (for the Mega65 emulator of Xemu).
--- navigator/navigator.asm
+++ navigator/navigator.asm
@@ -1,3 +1,21 @@
+; System/file navigator for Mega-65
+;
+; Copyright (C)2017 LGB (Gábor Lénárt) <lgblgblgb@gmail.com>
+;
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 .SETCPU "4510"	; This requires quite new CA65 (from CC65 suite) version, maybe git version only can do this ...
 
 .SEGMENT "LOADADDR"
